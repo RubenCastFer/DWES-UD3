387c5f81ff80a7d3823d505d5136f92d198c358d
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2021-10-04 11:44:08 +0200

    ejercicio 7

---
+++ ejercicio6.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" cellpadding="0" cellspacing="0" background="red" >
+        <tr bgcolor= 'red'>
+            <th>
+            <font color = white> Elevado a 
+            </th>
+            <th>
+            <font color = white> Resultado
+            </th>
+        <tr>
+
+        <?php
+            $limite = 10;
+            for ($i = 0; $i <= $limite; $i++ ){
+                $resultado = pow(2,$i);
+                echo "<tr bgcolor= 'red'>
+                        <th><font color = white> $i </th>
+                        <th><font color = white>  $resultado </th> 
+                    <tr>";
+            }
+        ?>
+    </table>
+   
+</body>
+</html>
